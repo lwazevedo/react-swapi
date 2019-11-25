80c73fc444dae9c78cff134cdab9569a6022dc78
pssofluiz <luiz.azevedo@topaziopartner.com.br>
2019-11-25 19:48:32 -0300

    Ajusted component btnDetails css

--- src/Components/BtnDetails.js
+++ src/Components/BtnDetails.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {identifyType} from '../Config/Constants';
 
 const BtnDetails = ({ listBtns, onDetail, active }) => {
   return (
     <div className='row'>
       <div className='col-sm-12'>
         {listBtns.map((item, key) => (
-          <button key={`i-${key}-${item}`} onClick={() => onDetail(item.toLowerCase())} className={`${active.toLowerCase() === item.toLowerCase() ? 'btn btn-primary hover-color-swapi-reverse btn-sm bg-transparent border-white text-color-swapi ml-3 active' : 'btn btn-primary hover-color-swapi-reverse btn-sm bg-transparent border-white text-color-swapi ml-3'}`}>{item}</button>
+          <button key={`i-${key}-${item}`} onClick={() => onDetail(item.toLowerCase())} className={`${active.toLowerCase() === identifyType[item.toLowerCase()] ? 'btn btn-primary hover-color-swapi-reverse btn-sm bg-transparent border-white text-color-swapi ml-3 active' : 'btn btn-primary hover-color-swapi-reverse btn-sm bg-transparent border-white text-color-swapi ml-3'}`}>{item}</button>
         ))}
       </div>
     </div >
